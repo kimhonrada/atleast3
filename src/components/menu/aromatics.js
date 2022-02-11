4716f39252ca4378669c47fcdc481380af20fe63
Kim Honrada <kimhonrada21@gmail.com>
2022-02-11 14:20:33 -0500

    add working drop down list for ingredients

--- src/components/menu/aromatics.js
+++ src/components/menu/aromatics.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Aromatics = () => {
+  const [aromatic, setAromatic] = useState("banana");
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setAromatic(e.target.value);
+  };
+
   return (
     <>
       <label htmlFor='aromaticsForm' className='form-label'>
         aromatics
       </label>
-      <select className='form-select' aria-label='Default select example'>
-        <option value='1'>One</option>
-        <option value='2'>Two</option>
-        <option value='3'>Three</option>
+      <p>current choice is {aromatic}</p>
+      <select
+        className='form-select'
+        aria-label='Default select example'
+        onChange={handleChange}
+      >
+        <option value='garlic'>Garlic</option>
+        <option value='onion'>Onion</option>
+        <option value='ginger'>Ginger</option>
       </select>
     </>
   );
