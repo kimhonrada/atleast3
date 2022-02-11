8956d84b7aecfae044399146ea6f84670ac71f31
Kim Honrada <kimhonrada21@gmail.com>
2022-02-11 13:25:44 -0500

    Add initial menu

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>Hello {name}</h1>
-        <button type='button' class='btn btn-primary'>
-          This is a bootstrap button
-        </button>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <h1>at least 3</h1>
+      <label htmlFor='aromaticsForm' className='form-label'>
+        aromatics
+      </label>
+      <select className='form-select' aria-label='Default select example'>
+        <option value='1'>One</option>
+        <option value='2'>Two</option>
+        <option value='3'>Three</option>
+      </select>
+      <label htmlFor='proteinForm' className='form-label'>
+        protein
+      </label>
+      <select className='form-select' aria-label='Default select example'>
+        <option value='1'>One</option>
+        <option value='2'>Two</option>
+        <option value='3'>Three</option>
+      </select>
+      <label htmlFor='veggieForm' className='form-label'>
+        veggies
+      </label>
+      <select className='form-select' aria-label='Default select example'>
+        <option value='1'>One</option>
+        <option value='2'>Two</option>
+        <option value='3'>Three</option>
+      </select>
+    </>
+  );
+};
 
 export default App;
