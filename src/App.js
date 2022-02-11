fe75c55c79ac777bfa2c8ac6cf58ac59a2fd5c21
Kim Honrada <kimhonrada21@gmail.com>
2022-02-11 13:33:07 -0500

    add individual menu form for ingredients

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Aromatics from "/src/components/menu/aromatics.js";
+import Veggies from "/src/components/menu/veggies.js";
+import Protein from "/src/components/menu/protein.js";
 
 const App = () => {
   return (
     <>
       <h1>at least 3</h1>
-      <label htmlFor='aromaticsForm' className='form-label'>
-        aromatics
-      </label>
-      <select className='form-select' aria-label='Default select example'>
-        <option value='1'>One</option>
-        <option value='2'>Two</option>
-        <option value='3'>Three</option>
-      </select>
-      <label htmlFor='proteinForm' className='form-label'>
-        protein
-      </label>
-      <select className='form-select' aria-label='Default select example'>
-        <option value='1'>One</option>
-        <option value='2'>Two</option>
-        <option value='3'>Three</option>
-      </select>
-      <label htmlFor='veggieForm' className='form-label'>
-        veggies
-      </label>
-      <select className='form-select' aria-label='Default select example'>
-        <option value='1'>One</option>
-        <option value='2'>Two</option>
-        <option value='3'>Three</option>
-      </select>
+      <Aromatics />
+      <Protein />
+      <Veggies />
     </>
   );
 };
---
+++ src/components/menu/veggies.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Veggies = () => {
+  return (
+    <>
+      <label htmlFor='veggiesForm' className='form-label'>
+        veggies
+      </label>
+      <select className='form-select' aria-label='Default select example'>
+        <option value='1'>One</option>
+        <option value='2'>Two</option>
+        <option value='3'>Three</option>
+      </select>
+    </>
+  );
+};
+
+export default Veggies;
